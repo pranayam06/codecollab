6a754a0abb9152797f5f7d774ed357f64e79faf7
Pranaya Murugan <muruganpranaya@gmail.com>
2025-01-06 21:25:28 -0500

    app.js added back and working code with textarea and updated chrome

--- app/app.js
+++ app/app.js
@@ -1,15 +1,9 @@
-console.log("app.js is loaded");
-
-const socket = new WebSocket('ws://localhost:3000')   
-
-var textarea = document.querySelector("textarea");
-var p = document.querySelectoruerySelector("p")
-
-function deltaCode(e) { 
-
-    p.innerText = e.target.value; 
-    console.log(e.target.value)
-
+    
+const input = document.querySelector("textarea");
+const log = document.getElementById("values");
+    
+input.addEventListener("input", updateValue);
+    
+function updateValue(e) {
+    log.textContent = e.target.value;
 }
-
-textarea.addEventListener('input', deltaCode);
