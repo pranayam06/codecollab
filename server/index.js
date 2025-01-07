65b62d165c9a7a8aa94c17960c873e2e7c711404
Pranaya Murugan <muruganpranaya@gmail.com>
2025-01-07 17:01:03 -0500

    integrating socket.io to index.js and adding script tag on client side

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
-const ws = require('ws')
-const server = new ws.Server({ port: '3000'}) 
+import { createServer } from "http"
+import { Server } from "socket.io"
+const httpServer = createServer()
+const io = new Server(httpServer, {
+    cors: {
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500"]
+    }
+})
 
-server.on('connection', socket => {
-    socket.on('message', message => {
-        const b = Buffer.from(message)
-        console.log(b.toString())
-        socket.send(`${message}`)
+io.on('connection', socket => {
+    console.log(`User: ${socket.id} connected`)
+    socket.on('message', data => {  
+        console.log(data)
+        
+        io.emit('message, '`${data}`)
 
     })
-} )
+} ) 
+
+httpServer.listen(3500, () => console.log("listening on port 3500"));
