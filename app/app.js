d94a0055d81a9632b9e3ff410ee335dee2931e86
Pranaya Murugan <muruganpranaya@gmail.com>
2025-01-07 16:12:23 -0500

    final commit for first iteration, refer to for ui and notes only

--- app/app.js
+++ app/app.js
@@ -1,9 +1,19 @@
-    
+const socket = new WebSocket('ws://localhost:3000')
+
 const input = document.querySelector("textarea");
 const log = document.getElementById("values");
     
 input.addEventListener("input", updateValue);
     
 function updateValue(e) {
-    log.textContent = e.target.value;
+    log.textContent = e.target.value; 
+    socket.send(e.target.value);
 }
+
+socket.addEventListener("message", ({ data }) => { 
+    console.log("socket message recieved")
+    console.log(data)
+    input.textContent = data;
+    log.textContent = data; 
+
+})
