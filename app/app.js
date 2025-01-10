17d8d5d777edd7284019e3264fa8df0686938124
Pranaya Murugan <muruganpranaya@gmail.com>
2025-01-09 23:38:50 -0500

    working users connect/disconnect sockets

--- app/app.js
+++ app/app.js
@@ -16,4 +16,29 @@ socket.on("message", (data) => {
     input.value = data;
     log.textContent = data; 
 
+}) 
+
+socket.on("user connected", (data) => { 
+
+    console.log(data + "user connected") 
+    let list = document.getElementById("collaborators")  
+    let newId = document.createElement("li")
+    newId.innerText = data
+    list.appendChild(newId) 
+
+
 })
+
+socket.on("rerender collaborators", (data) => {  
+    let list = document.getElementById("collaborators")   
+    list.innerHTML = ""; 
+
+
+    for (const col of data){ 
+        let newId = document.createElement("li")
+        newId.innerText = col
+        list.appendChild(newId) 
+
+    }
+}
+)
--- server/index.js
+++ server/index.js
@@ -5,10 +5,26 @@ const io = new Server(httpServer, {
     cors: {
         origin: process.env.NODE_ENV === "production" ? false : ["http://127.0.0.1:5500"]
     }
-})
+}) 
+let collaborators = []
 
-io.on('connection', socket => {
-    console.log(`User: ${socket.id} connected`)
+io.on('connection', socket => {   
+    collaborators.push(socket.id)   
+    console.log(`User: ${socket.id} connected`)  
+
+    socket.on("disconnect", (reason) => { 
+        var index = collaborators.indexOf(`${socket.id}`)
+        collaborators.splice(index, 1) 
+        io.emit("rerender collaborators", collaborators)
+    })
+    
+    io.emit("rerender collaborators" , collaborators)
+
+    
+
+    
+
+
     socket.on('message', data => {  
         console.log(data)
         
